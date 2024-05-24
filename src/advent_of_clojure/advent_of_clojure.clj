460e39aa14eb7399c0b8c6400933720eb2705137
Jack Mocherman <jackmocherman@Jacks-MacBook-Pro.local>
2024-05-24 09:04:25 -0500

    trigger more linting warnings

--- src/advent_of_clojure/advent_of_clojure.clj
+++ src/advent_of_clojure/advent_of_clojure.clj
@@ -1,5 +1,6 @@
 (ns advent-of-clojure.advent-of-clojure
-  (:require [clojure.set :as s]))
+  (:require [clojure.set :as s]
+            [clojure.string :as str]))
 
 (defn -main
   "Invoke me with clojure -M -m advent-of-clojure.advent-of-clojure"
